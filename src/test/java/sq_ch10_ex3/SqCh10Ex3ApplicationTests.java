00a3a128743e9dad2e515511ef63f0a1968f60bb
osparking <jbpark03@gmail.com>
2023-10-15 20:52:38 +0900

    initial with lombok and web depend'

---
+++ src/test/java/sq_ch10_ex3/SqCh10Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sq_ch10_ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh10Ex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
